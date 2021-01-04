4f1af8a86893dc212a945a39057006b728d99ddf
Vikas Semwal <vikas.semwal1994@gmail.com>
2021-01-04 19:28:02 +0530

    First commit

---
+++ app/src/main/java/com/example/recyclerview/RecyclerAdapter.java
@@ -0,0 +1,95 @@
+package com.example.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerAdapter";
+    List<String> postList;
+    private RecyclerViewClickInterface recyclerViewClickInterface;
+
+    public RecyclerAdapter(List<String> moviesList, RecyclerViewClickInterface recyclerViewClickInterface) {
+        this.postList = moviesList;
+        this.recyclerViewClickInterface = recyclerViewClickInterface;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.rowCountTextView.setText(String.valueOf(position));
+        holder.textView.setText(postList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView textView, rowCountTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+            rowCountTextView = itemView.findViewById(R.id.rowCountTextView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    recyclerViewClickInterface.onItemClick(getAdapterPosition());
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+//                    postList.remove(getAdapterPosition());
+//                    notifyItemRemoved(getAdapterPosition());
+
+                    recyclerViewClickInterface.onLongItemClick(getAdapterPosition());
+
+                    return true;
+                }
+            });
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
